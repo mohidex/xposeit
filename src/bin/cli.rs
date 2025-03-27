4d45c85d9e2d8cc331f018578965911e55d29c1f
Mohidul Islam <mohidul.cs@gmail.com>
2025-03-27 20:59:04 +0100

    removed additoinal subcommand as bins are seperated

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,49 +1,30 @@
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use xposeit::XposeCli;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    #[clap(subcommand)]
-    command: Command,
-}
+    /// The local port to expose.
+    #[clap(short, long)]
+    port: u16,
 
-pub enum Commands {
-    Cli(Box<XposeCli>),
-}
+    /// The local host to expose.
+    #[clap(short, long, value_name = "LOCAL_HOST", default_value = "localhost")]
+    local_host: String,
 
-#[derive(Subcommand, Debug)]
-enum Command {
-    /// Starts a local proxy to the remote server.
-    Xpose {
-        /// The local port to expose.
-        #[clap(short, long)]
-        port: u16,
-
-        /// The local host to expose.
-        #[clap(short, long, value_name = "LOCAL_HOST", default_value = "localhost")]
-        local_host: String,
-
-        /// Address of the remote server to expose local ports to.
-        #[clap(short, long, value_name = "REMOTE_HOST", default_value = "localhost")]
-        to: String,
-    },
+    /// Address of the remote server to expose local ports to.
+    #[clap(short, long, value_name = "REMOTE_HOST", default_value = "localhost")]
+    to: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    match Args::parse().command {
-        Command::Xpose {
-            local_host,
-            port,
-            to,
-        } => {
-            let client = XposeCli::new(&local_host, port, &to).await?;
-            client.listen().await?;
-        }
-    }
+    let args = Args::parse();
+    let client = XposeCli::new(&args.local_host, args.port, &args.to).await?;
+    client.listen().await?;
+
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,46 +1,34 @@
 use anyhow::Result;
-use clap::{error::ErrorKind, CommandFactory, Parser, Subcommand};
+use clap::{error::ErrorKind, CommandFactory, Parser};
 use xposeit::XposeServer;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    #[clap(subcommand)]
-    command: Command,
-}
+    /// Minimum accepted TCP port number.
+    #[clap(long, default_value_t = 1024)]
+    min_port: u16,
 
-pub enum Commands {
-    Server(Box<XposeServer>),
-}
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    /// Runs the remote proxy server.
-    Server {
-        /// Minimum accepted TCP port number.
-        #[clap(long, default_value_t = 1024)]
-        min_port: u16,
-
-        /// Maximum accepted TCP port number.
-        #[clap(long, default_value_t = 65535)]
-        max_port: u16,
-    },
+    /// Maximum accepted TCP port number.
+    #[clap(long, default_value_t = 65535)]
+    max_port: u16,
+    
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    match Args::parse().command {
-        Command::Server { min_port, max_port } => {
-            let port_range = min_port..=max_port;
-            if port_range.is_empty() {
-                Args::command()
-                    .error(ErrorKind::InvalidValue, "port range is empty")
-                    .exit();
-            }
-            XposeServer::new(port_range).listen().await?;
-        }
+    let args = Args::parse();
+    let port_range = args.min_port..=args.max_port;
+    if port_range.is_empty() {
+        Args::command()
+            .error(ErrorKind::InvalidValue, "port range is empty")
+            .exit();
     }
+
+    let server = XposeServer::new(port_range);
+    server.listen().await?;
+
     Ok(())
 }
